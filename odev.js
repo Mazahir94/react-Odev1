7ae5dcaa1d0592264ff038ad7d0ace69d4a0c76c
Mazahir94 <mazahir.pashaev@gmail.com>
2022-08-13 12:53:14 +0400

    odev1 finished

--- odev.js
+++ odev.js
@@ -1,1 +1,10 @@
-gfd
+import axios from "axios";
+
+async function getData () {
+const {data: users} = await axios ("https://jsonplaceholder.typicode.com/users/1");
+const {data: posts} = await axios ("https://jsonplaceholder.typicode.com/posts/1");
+console.log( users);
+console.log( "posts", posts);
+}
+
+getData();
